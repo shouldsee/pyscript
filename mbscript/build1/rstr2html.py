d90696dfb743931609d3a46279472f5c786451c3
shouldsee <shouldsee.gem@gmail.com>
2017-03-21 20:34:44 +0000

    build1

--- mbscript/build1/rstr2html.py
+++ mbscript/build1/rstr2html.py
@@ -12,26 +12,39 @@ file_ipynb='calc_temp.ipynb';
 query = sys.argv[1:];
 # except:
 query = query[0];
+file_html='temp_'+query+'.html';
 if not query: 
 	print('query not recognised')
 	quit()
+print(query)
 environ['query'] = query;
-call(["runipy", file_ipynb])
-file_html='temp_'+query+'.html';
+# call(['ls','-l'])
+# ags=["runipy",'--html '+file_html];
+# ags=["runipy",'--html '+file_html];
+# ags=["runipy",file_ipynb,'temp.ipynb']
+# ags=["runipy",
+# 	"-o",'temp.ipynb',
+# 	"--html",'temp.html']
+# print(' '.join(ags));
 
-notebook = read(open(file_ipynb), 'json')
-r = NotebookRunner(notebook)
-r.run_notebook()
+ags=["runipy",file_ipynb,'temp.ipynb']
+call(ags);
+
+
+# notebook = read(open(file_ipynb), 'json')
+# r = NotebookRunner(notebook)
+# r.run_notebook()
 
 from nbconvert import HTMLExporter
 import codecs
 import nbformat
 exporter = HTMLExporter()
-# execfile()
-from IPython.nbformat.current import write
-write(r.nb, open("temp.ipynb", 'w'), 'json')
+# # execfile()
+# from IPython.nbformat.current import write
+# write(r.nb, open("temp.ipynb", 'w'), 'json')
 
 output_notebook = nbformat.read("temp.ipynb", as_version=4)
-# output, resources = exporter.from_notebook_node(output_notebook)
 output, resources = exporter.from_notebook_node(output_notebook)
+# output, resources = exporter.from_notebook_node(r.nb);
+# output, resources = exporter.from_notebook_node(output_notebook)
 codecs.open(file_html, 'w', encoding='utf-8').write(output)
